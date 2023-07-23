db820ee5a8668c6c89ce8213ff28534247599e9f
Lostovayne <moises.barrera.dev@gmail.com>
2023-07-23 01:00:02 -0400

    agregado arreglo de rutas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import logo from "../assets/react.svg";
+import { routes } from "./routes";
 
 const Navigation = () => {
     return (
@@ -9,44 +10,22 @@ const Navigation = () => {
                     <img src={logo} alt="React logo" width={150} height={150} />
 
                     <ul>
-                        <li>
-                            <NavLink
-                                to="/home"
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/about"
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                            >
-                                About
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/users"
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                            >
-                                Users
-                            </NavLink>
-                        </li>
+                        {routes.map((route) => (
+                            <li key={route.path}>
+                                <NavLink to={route.path} className={({ isActive }) => (isActive ? "nav-active" : "")}>
+                                    {route.name}
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="/about" element={<h1>About Page</h1>} />
-                    <Route path="/users" element={<h1>Users Page</h1>} />
-                    <Route path="/home" element={<h1>Home Page</h1>} />
-                    <Route path="/*" element={<h1>Page Not Found</h1>} />
+                    {routes.map(({ path, Component }) => (
+                        <Route key={path} path={path} element={<Component />} />
+                    ))}
+
+                    <Route path="/*" element={<Navigate to={routes[0].path} />} />
                 </Routes>
             </div>
         </BrowserRouter>
